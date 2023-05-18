b5646540c1eedc7e0b9b78edf6a152b9d7944ecc
av.makarova1 <av.makarova1@tensor.ru>
2023-05-18 12:05:16 +0300

    Чух, чух, чух, ту, ту, ту – Белые барашки Выдувает на ходу Паровоз – Букашка!Чух, чух, чух, ту, ту, ту – Белые барашки Выдувает на ходу Паровоз – Букашка!

--- задание 5/task_3.py
+++ задание 5/task_3.py
@@ -22,7 +22,7 @@ def everything_for_your_cat(cats_data):
     our_str = {}  # Пустой словарь
     for cat in cats_data:
         temp = cat[0] + ', ' + str(cat[1])
-        #  методом словарей setdefault() и методом join() объединим элементы списков и кортежей
+        #  Методом словарей setdefault() и методом join() объединим элементы списков и кортежей
         #  с необходимыми разделителями:
         our_str.setdefault(cat[2:], []).append(temp)
     our_str = ''.join(f'{p[0]} {p[1]}: {"; ".join(j)}\n' for p, j in our_str.items())
